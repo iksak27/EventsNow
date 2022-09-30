cd5af12ac4cca8927d9166103e83e2710cf23ce3
iksak27 <103168776+iksak27@users.noreply.github.com>
2022-09-30 01:50:42 -0400

    error handling and initial redux

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import eventRoutes from './routes/eventRoutes.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -11,8 +12,9 @@ connectDB()
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
-
 app.use('/api/events', eventRoutes)
+app.use(errorHandler)
+
 
 const port = process.env.PORT || 5500
 app.listen(port, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${port}`))
